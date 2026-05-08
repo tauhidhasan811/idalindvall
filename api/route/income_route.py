923201b5929b37d2ad351256108bbf11d162b23a
tauhidhasan811 <tauhidhasan811@gmail.com>
2026-05-08 10:28:37 +0600

    successfully get response from claude

--- api/route/income_route.py
+++ api/route/income_route.py
@@ -1,9 +1,26 @@
 from fastapi import APIRouter
 from api.schema.chat_schema import ChatSchema
+from fastapi.responses import StreamingResponse
+from src.service.income_service import IncomeService
+from src.core.chat_with_anthropic import ChatController
+
 
 router = APIRouter(prefix='/api/ai/income', tags=['Income'])
 
-@router.post('/create-session')
+chat_model = ChatController()
+
+
+@router.post('/chat')
 async def create_new_session(chat_data: ChatSchema):
-    return 
+    chat_history_dict = [item.dict() for item in chat_data.chat_history]
+    response = IncomeService.analysis_chat(chat_history=chat_history_dict, chat_model=chat_model)
+
+    # response = chat_model.get_response(prompt=prompt)
+
+    return response
+
+
+    # chat_model.get_response(prompt=prompt)
+    # return StreamingResponse(chat_model.get_response(prompt=prompt), media_type='text/event-stream')
+
     
--- src/service/income_service.py
+++ src/service/income_service.py
@@ -1,3 +1,17 @@
 from src.core.session import Session
+from src.core.data_processor import ProcessData
+from src.core.generate_prompt import GeneratePrompt
 
-# class IncomeService
+
+
+class IncomeService:
+
+    @staticmethod
+    def analysis_chat(chat_history: list[dict], chat_model):
+        last_message, history = ProcessData.process_chat_history(chat_history=chat_history)
+        prompt = GeneratePrompt.income_prompt(last_chat=last_message, previous_history=history)
+        response = chat_model.get_response(prompt=prompt)
+        print(type(response))
+        clean_response = ProcessData.CleanData(response.content)
+        return clean_response
+
